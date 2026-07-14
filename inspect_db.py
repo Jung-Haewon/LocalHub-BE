4faacac3d9a43c464d53453a37e38b324b650c7c
Minjun Kim <minjun.kim.per@gmail.com>
2026-07-14 17:25:02 +0900

    Clean up code

--- inspect_db.py
+++ inspect_db.py
@@ -6,17 +6,6 @@ from chatbot.chatbot import Chatbot
 
 retriever = retriever.Retriever("localhub.db") 
 
-query = "선릉 주변 모텔 어딨는지 알려줘링 링딩동 동귀리리"
-
-parsed = keyword_parser.parse_question(query)
-
-
-results = retriever.retrieve(parsed)
-for result in results:
-    print(f"Result: {result}")
-
-prompt = prompt.build_prompt(query, results)
-
 bot = Chatbot("localhub.db")
 
 while True:
