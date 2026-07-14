d951faf7a8005ec05b5c199345454482e4a763ea
Minjun Kim <minjun.kim.per@gmail.com>
2026-07-14 16:24:01 +0900

    Test query based retrieval functionality

--- inspect_db.py
+++ inspect_db.py
@@ -1,13 +1,23 @@
 import sqlite3
+from chatbot import keyword_parser
+from chatbot import retriever
+
+retriever = retriever.Retriever("localhub.db") 
+
+query = "선릉 주변 모텔 어딨는지 알려줘링 링딩동 동귀리리"
+
+parsed = keyword_parser.parse_question(query)
+
+
+results = retriever.retrieve(parsed)
+for result in results:
+    print(f"Result: {result}")
 
 conn = sqlite3.connect("localhub.db")
 cur = conn.cursor()
 
-cur.execute("SELECT title FROM locations;")
+cur.execute("SELECT addr1 FROM locations;")
 rows = cur.fetchall()
 
-print("Locations:")
-for row in rows:
-    print("-", row[0])
-
 conn.close()
+
